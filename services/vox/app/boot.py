9dcf22f2be5474beaa0e07dada62ec419a33da32
enborra <andres@enborra.com>
2016-10-03 00:54:11 -0700

    functional voice detection and centralstation publishing on trigger keyword

--- services/vox/app/boot.py
+++ services/vox/app/boot.py
@@ -1,5 +1,8 @@
 import os
+import time
 from pocketsphinx import LiveSpeech, get_model_path, Decoder
+import paho.mqtt.client as mqtt
+
 
 path_lm = '/Users/andres/projects/turing/services/vox/app/detection_model/base.lm'
 path_dict = '/Users/andres/projects/turing/services/vox/app/detection_model/base.dic'
@@ -36,7 +39,44 @@ model_path = get_model_path()
 # base.lm - language model file
 # base.dic - word dictionary file that scopes pronunciation
 
+_comm_client = mqtt.Client()
 
+def _on_connect(client, userdata, flags, rc):
+    print "New connection: " + str(rc)
+
+    _comm_client.subscribe('system', 0)
+
+
+def _on_message(client, userdata, msg):
+    print 'GOT MESSAGE (qos=' + str(msg.qos) + ', topic=' + str(msg.topic) + '): ' + str(msg.payload)
+
+    _face.blink()
+
+def _on_publish(mosq, obj, mid):
+    print 'mid: ' + str(mid)
+
+def _on_subscribe(mosq, obj, mid, granted_qos):
+    print 'Subscribed: ' + str(mid) + ' ' + str(granted_qos)
+
+def _on_log(mosq, obj, level, string):
+    print 'Log: ' + str(string)
+
+
+
+_comm_client.on_message = _on_message
+_comm_client.on_connect = _on_connect
+_comm_client.on_publish = _on_publish
+_comm_client.on_subscribe = _on_subscribe
+
+try:
+    _comm_client.connect('localhost', 1883, 60)
+    print('Connected to centralstation')
+
+except Exception, e:
+    print('Couldn\'t connect to centralstation. Retrying in 1 second.')
+
+    time.sleep(1)
+    self._connect_to_comms()
 
 
 import sys, os
@@ -75,16 +115,20 @@ while True:
         decoder.end_utt()
         stream.stop_stream()
 
+        _comm_client.loop()
+
         if str(hypothesis.hypstr).lower().strip() == 'swell':
             try:
+                _comm_client.publish('vox', 'swell')
                 output = subprocess.check_output('afplay audio/r2d2.wav', shell=True)
-                pass
+
             except Exception:
                 pass
         elif str(hypothesis.hypstr).lower().strip() == 'dolly':
             try:
+                _comm_client.publish('vox', 'dolly')
                 output = subprocess.check_output('afplay audio/bb8.mp3', shell=True)
-                pass
+
             except Exception:
                 pass
         else:
