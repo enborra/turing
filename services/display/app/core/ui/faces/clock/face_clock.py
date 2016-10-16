5a95046749485e604d9638305c17490247d887bb
enborra <andres@enborra.com>
2016-10-15 21:52:03 -0700

    .

--- services/display/app/core/ui/faces/clock/face_clock.py
+++ services/display/app/core/ui/faces/clock/face_clock.py
@@ -6,6 +6,7 @@ from core.ui.faces.dali.eye import Eye
 
 class ClockFace(object):
     renderer = None
+    i = 0
 
 
     def __init__(self, parent_renderer=None):
@@ -15,6 +16,11 @@ class ClockFace(object):
         pass
 
     def render(self):
+        if i>200:
+            i = 0
+        else:
+            i += 1
+
         if self.renderer:
             self.renderer.rectangle((0,0,240,320), fill=(0,0,0))
-            self.renderer.rectangle((100,100,150,150), fill=(0,255,0))
+            self.renderer.rectangle((i,100,150,150), fill=(0,255,0))
