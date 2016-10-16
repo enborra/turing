f0ff71df25496866ea7d64fbe8be0939bfc85508
enborra <andres@enborra.com>
2016-10-15 22:14:34 -0700

    .

--- services/display/app/core/ui/faces/clock/face_clock.py
+++ services/display/app/core/ui/faces/clock/face_clock.py
@@ -24,4 +24,5 @@ class ClockFace(object):
         if self.renderer:
             self.renderer.rectangle((0,0,240,320), fill=(0,0,0))
             self.renderer.rectangle((100,self.i,150,self.i+50), fill=(0,255,0))
-            self.renderer.line((0, 0, 100, 100), fill=128)
+
+            self.text((0, 0), "Hello", (255,255,255))
