1bb122a3211983f8699c04b13ab43505be9415ce
enborra <andres@enborra.com>
2016-12-28 22:16:45 -0800

    fixing pi cli setup

--- system/services/cli/app/core/controller_raspberry_pi.py
+++ system/services/cli/app/core/controller_raspberry_pi.py
@@ -19,8 +19,8 @@ class RaspberryPiController(BaseController):
         # If the service is running, stop it
 
         try:
-            self.run_command('sudo systemctl status %s' % current_run_file_name)
-            self.run_command('sudo systemctl stop %s' % current_run_file_name)
+            self.run_command('sudo systemctl status %s' % (self._path_run_directory + current_run_file_name))
+            self.run_command('sudo systemctl stop %s' % (self._path_run_directory + current_run_file_name))
 
             output_msg = '{{RED}}Stopped service:{{WHITE}} %s' % current_name
 
