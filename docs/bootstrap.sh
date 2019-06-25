c2ee3e21c9b02909d7fb73187d41ea43896822e7
Andres <andres@enborra.com>
2019-06-25 12:15:48 -0700

    Update bootstrap.sh

--- docs/bootstrap.sh
+++ docs/bootstrap.sh
@@ -1,1 +1,10 @@
 # Run this file remote to kick off install
+
+# Check for git on system
+## Install if git doesn't exist
+
+# Check for python on system
+## Install if python doesn't exist
+
+# Check for nodejs on system
+## Install if nodejs doesn't exist
