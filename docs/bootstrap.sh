a56d5e11f0de525222b31deb0c28f69acd252c47
Andres <andres@enborra.com>
2019-07-09 21:19:39 -0700

    adding initial boostrap script

---
+++ docs/bootstrap.sh
@@ -0,0 +1,6 @@
+sudo mkdir /etc/turing
+sudo mkdir /etc/turing/droids
+sudo mkdir /etc/turing/outputs
+sudo mkdir /etc/turing/services
+
+git clone https://github.com/enborra/turing.git /etc/turing/framework
