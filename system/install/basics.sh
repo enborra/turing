2fc3ed9963b98c20cf64912173213e0c4be6f1fa
Andres <andres@enborra.com>
2020-06-26 18:11:27 -0700

    Create basics.sh

---
+++ system/install/basics.sh
@@ -0,0 +1,9 @@
+# Install NVM
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+source ~/.bashrc  # Rerun profile after installing nvm
+
+# Install Node
+nvm install 8  # Installs Node v8, (nvm install stable) installs Latest version of node
+nvm use 8  # Sets Node to use v8
+
+npm install npm@latest -g
