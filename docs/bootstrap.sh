00fb6fdd134b111017d3ca812847184a839153ff
Andres <andres@enborra.com>
2020-06-27 01:12:51 -0700

    Update bootstrap.sh

--- docs/bootstrap.sh
+++ docs/bootstrap.sh
@@ -17,6 +17,10 @@ sudo mkdir /etc/turing/droids
 sudo mkdir /etc/turing/outputs
 sudo mkdir /etc/turing/services
 
+# Write an example config.json
+
+echo "{\"current-droid\":\"oswald\",\"droid-path\":\"/etc/turing/droids\",\"service-path\":\"/etc/turing/services\"}" > /etc/turing/config.json
+
 # Clone the Turing repo (this repo) to the local system at the /etc location
 
 sudo git clone https://github.com/enborra/turing.git /etc/turing/framework
